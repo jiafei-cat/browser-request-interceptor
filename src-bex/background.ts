8683315a2d1deec7cf0173177aad0e94c625b914
garfield <yinjianihao@qq.com>
2023-07-07 16:23:52 +0800

    fix: 修复通信问题

--- src-bex/background.ts
+++ src-bex/background.ts
@@ -1,3 +1,4 @@
+import { BexBridge } from '@quasar/app-vite'
 import { bexBackground } from 'quasar/wrappers'
 
 declare module '@quasar/app-vite' {
@@ -5,35 +6,47 @@ declare module '@quasar/app-vite' {
     /* eslint-disable @typescript-eslint/no-explicit-any */
     log: [{ message: string; data?: any[] }, never]
     getTime: [never, number]
-
-    'storage.get': [{ key: string | null }, any]
-    'storage.set': [{ key: string; value: any }, any]
-    'storage.remove': [{ key: string }, any]
+    getTabId: [number]
     /* eslint-enable @typescript-eslint/no-explicit-any */
   }
 }
 
 const installedTabIds: (string | number)[] = []
+console.log('background running')
 
-/** chrome.runtime.onConnect 时运行 */
+/** 防止多次监听 */
+let isListened = false
+
+/**
+ * chrome.runtime.onConnect 时运行
+ * 在进入一个tab时, quasar会执行chrome.runtime.connect({ name: "contentScript" }) 从而触发bexBackground
+ * 这里的bridge会和当前connect进行通信
+ */
 export default bexBackground((bridge, activeConnections) => {
-  const [activeId] = Object.keys(activeConnections)
+  console.log('[bexBackground] connect content', activeConnections)
+
+  const [curTabId] = Object.keys(activeConnections)
+
+  bridge.on('getTabId', async ({ data, respond }) => {
+    const result = await chrome.tabs.query({ active: true, currentWindow: true })
+    respond(result[0].id)
+  })
 
   bridge.on('sendRequestList', ({ data }) => {
     console.log('background sendRequestList')
     bridge.send('getRequestList', data)
   })
 
-  /** 防止以下监听多次绑定 */
-  // if (installedTabIds.includes(activeId)) {
-  //   console.log('已安装')
-  //   return
-  // }
+  if (isListened) {
+    return
+  }
 
-  chrome.action.onClicked.addListener(() => {
-    console.log('background toggleShow')
-    bridge.send('toggleShow')
+  isListened = true
+
+  chrome.action.onClicked.addListener(async () => {
+    console.log('chrome background toggleShow')
+    const data = await chrome.tabs.query({ active: true, currentWindow: true })
+
+    bridge.send('toggleShow', data[0].id)
   })
-
-  installedTabIds.push(activeId)
 })
--- src-bex/my-content-script.ts
+++ src-bex/my-content-script.ts
@@ -4,17 +4,24 @@
 import { bexContent } from 'quasar/wrappers'
 import type { IRequestItem } from 'src-bex/dom'
 
-console.log('执行 content.js')
+console.log('bexContent running')
+
 let isInstalled = false
 let iframe: HTMLIFrameElement | null = null
 let isIframeShow = true
-
 let lastRequestList: IRequestItem[] = []
-let handleSendRequestList: VoidFunction | null = null
+let curTabId: number | null = null
 
-export default bexContent((bridge) => {
+export default bexContent(async (bridge) => {
+  if (window.self !== window.top) {
+    return
+  }
+  const { data } = await bridge.send('getTabId')
+  curTabId = data
+  console.log('[bexContent] run', data)
+
   /** 转发给background */
-  bridge.on('sendRequestList', ({ data, respond }) => {
+  bridge.on('sendRequestList', ({ data }) => {
     lastRequestList = data
     if (!isInstalled) {
       return
@@ -22,68 +29,71 @@ export default bexContent((bridge) => {
     bridge.send('sendRequestList', lastRequestList)
   })
 
-  bridge.on('toggleShow', () => {
-    console.log('content toggleShow', isInstalled, iframe)
+  bridge.on('toggleShow', ({ data }) => {
+    console.log('content toggleShow', isInstalled, iframe, data, curTabId)
+    if (Number(curTabId) !== Number(data)) {
+      return
+    }
+
     if (isInstalled) {
       console.log('content toggleShow')
       toggleShowIframe()
       return
     }
-
     installIframe()
   })
 
   installIframe(false)
 
-  handleSendRequestList = () => {
+  const handleSendRequestList = () => {
     console.log('send message', lastRequestList)
     bridge.send('sendRequestList', lastRequestList)
   }
-})
 
-/** 切换iframe显示 */
-function toggleShowIframe() {
-  console.log('toggleShowIframe', iframe)
-  if (!iframe) {
-    return
+  /** 切换iframe显示 */
+  function toggleShowIframe() {
+    console.log('toggleShowIframe', iframe)
+    if (!iframe) {
+      return
+    }
+
+    if (isIframeShow) {
+      handleSendRequestList && handleSendRequestList()
+    }
+
+    iframe.style.transform = isIframeShow ? 'translateY(0)' : 'translateY(-120%)'
+    isIframeShow = !isIframeShow
   }
 
-  if (isIframeShow) {
-    handleSendRequestList && handleSendRequestList()
-  }
+  /** 嵌入iframe */
+  function installIframe(initialShow = true) {
+    const curPageState = document.readyState
+    const isPageNotLoaded = curPageState !== 'complete' && curPageState !== 'interactive'
 
-  iframe.style.transform = isIframeShow ? 'translateY(0)' : 'translateY(-120%)'
-  isIframeShow = !isIframeShow
-}
-
-function installIframe(initialShow = true) {
-  const curPageState = document.readyState
-  const isPageNotLoaded = curPageState !== 'complete' && curPageState !== 'interactive'
-
-  if (!isPageNotLoaded) {
-    createPopupIframe()
-    initialShow && toggleShowIframe()
-    return
-  }
-
-  document.onreadystatechange = () => {
-    if (document.readyState === 'interactive') {
+    if (!isPageNotLoaded) {
       createPopupIframe()
       initialShow && toggleShowIframe()
+      return
+    }
+
+    document.onreadystatechange = () => {
+      if (document.readyState === 'interactive') {
+        createPopupIframe()
+        initialShow && toggleShowIframe()
+      }
     }
   }
-}
 
-/** 创建iframe插入当前tabs */
-function createPopupIframe() {
-  /** 避免插入到子iframe中去，只在顶层插入 */
-  if (window.self !== window.top) {
-    return
-  }
+  /** 创建iframe插入当前tabs */
+  function createPopupIframe() {
+    /** 避免插入到子iframe中去，只在顶层插入 */
+    if (window.self !== window.top) {
+      return
+    }
 
-  iframe = document.createElement('iframe')
+    iframe = document.createElement('iframe')
 
-  iframe.style.cssText = `
+    iframe.style.cssText = `
     width: 518px;
     height: 100%;
     position: fixed;
@@ -96,9 +106,10 @@ function createPopupIframe() {
     transform: translateY(-120%);
     box-shadow: 0 0 10px rgba(0, 0, 0, .3);
   `
-  iframe.style.border = 'none'
-  iframe.src = chrome.runtime.getURL('www/index.html')
-  document.body.appendChild(iframe)
-  console.log('installed done')
-  isInstalled = true
-}
+    iframe.style.border = 'none'
+    iframe.src = chrome.runtime.getURL('www/index.html')
+    document.body.appendChild(iframe)
+    console.log('installed done')
+    isInstalled = true
+  }
+})
